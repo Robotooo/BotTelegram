4a17c9862602c489a42f96a73740c8d9995052cb
Robotooo <50878739+Robotooo@users.noreply.github.com>
2021-10-22 14:49:48 -0600

    bot

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+
+const TelegramBot = require('node-telegram-bot-api')
+const token = '2069797539:AAFWwLYZjrftI4tHtoNVBMNIUE8m3PT_zHU'
+const bot = new TelegramBot(token, {polling: true})
+var chatId;
+
+
+bot.onText(/\/echo (.+)/, (msg?:any, match?:any) => {
+	chatId = msg.chat.id
+	const resp = match[1]
+	bot.sendMessage(chatId, resp)
+})
+
+bot.onText(/\/info/, (msg?:any) => {
+	const chatId = msg.chat.id
+	bot.sendMessage(chatId, "No Info")
+})
